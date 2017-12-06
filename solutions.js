3ba70f1f8b5845db18ef25d37654cb350f7222fe
hoosieree <ashroyer@149-160-171-46.dhcp-bl.indiana.edu>
2017-12-06 17:47:08 -0500

    note c++ comparison to JS

--- solutions.js
+++ solutions.js
@@ -1,5 +1,6 @@
-// run in the JS console at http://adventofcode.com/2017/day/5/input
-[1/0,3].map(x=>(
+// paste into JS console at http://adventofcode.com/2017/day/5/input
+// runtime is about 150ms  (compare to C++:75ms, not too shabby)
+const d5 = [1/0,3].map(x=>(
   (a,j,c=0,i=0)=>{
     while(i>=0&&i<a.length){
       j=a[i];
@@ -8,8 +9,4 @@
       c++;
     }
     return c;
-  }) (document.getElementsByTagName('pre')[0].innerText.split('\n').map(Number).slice(0,-1))
-)
-// Execution times:
-// part1: 1.68ms
-// part2: 148.76ms
+  }) (document.getElementsByTagName('pre')[0].innerText.split('\n').map(Number).slice(0,-1)));
