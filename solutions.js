267e55f5b5824891038f4727f6b2868273fe33ef
hoosieree <ashroyer@gmail.com>
2017-12-26 16:02:32 -0500

    rename so ls sorts properly

--- solutions.js
+++ solutions.js
@@ -1,33 +1,16 @@
 // http://www2.unibas.it/gscanniello/Giuseppe_Scanniello%40unibas/Home_files/TOSEM.pdf
-let p=0, // pointer
-    gnl=0,
-    token='init',
-    state=[],
-    group=[];
-
-const inp=document.getElementsByTagName('pre')[0].innerText,
-      len=inp.length,
-      tok=(c)=>{
-        if(c==='!'){return 'ignore';}
-        if(c==='<'){return 'garb_start';}
-        if(c==='>'){return 'garb_end';}
-        if(c==='{'){return 'group_start';}
-        if(c==='}'){return 'group_end';}
-        return 'any';
-      };
-
-token = tok(inp[0]);
-console.log(`current state: ${token}`);
-
-state = [token];
-
-while(p<len){
-  token = tok(inp[p++]);
-  switch(token){
-  case'ignore': p++; break;
-  }
+const inp=document.getElementsByTagName('pre')[0].innerText;
+let ig=false,g=0,s=0,d=0,c=inp[0];
+for(let i=0; i<inp.length; ++i){
+  c=inp[i];
+  if(c==='!') i++;
+  else if(ig && c!=='>') g++;
+  else if(c==='<') ig=true;
+  else if(c==='>') ig=false;
+  else if(c==='{') s+=d++;
+  else if(c==='}') d--;
 }
-
+console.log(s,g);
 
 // paste into JS console at http://adventofcode.com/2017/day/5/input
 // runtime is about 150ms  (compare to C++:75ms, not too shabby)
@@ -41,7 +24,6 @@ while(p<len){
 //     }
 //     return c;
 //   }) (inp.split('\n').map(Number).slice(0,-1)));
-
 // const d7i = inp.split('\n').slice(0,-1);
 // const ne = d7i.map(x=>x.split(' -> '));  // node, edges
 // const o={};
@@ -49,5 +31,3 @@ while(p<len){
 //   let [n, w] = x[0].split(' ');
 //   o[n] = {weight: eval(w), kids: x[1] && x[1].split(', ')};
 // });
-
-
